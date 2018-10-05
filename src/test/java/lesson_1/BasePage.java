d26331cb48c74ea4d8999c57aa04913d29514e53
Your Name <mmuntakim15@gmail.com>
2018-10-05 18:13:13 -0400

    added wait

--- src/test/java/lesson_1/BasePage.java
+++ src/test/java/lesson_1/BasePage.java
@@ -1,15 +1,41 @@
 package lesson_1;
 
-import org.openqa.selenium.By;
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.google.common.base.Function;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
 
+    // This is the most common wait function used in selenium
+    public static WebElement webAction(final By locator) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(DriverWrapper.getDriver())
+                .withTimeout(15, TimeUnit.SECONDS)
+                .pollingEvery(1, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(ElementNotFoundException.class)
+                .withMessage(
+                        "Webdriver waited for 15 seconds but still could not find the element therefore Timeout Exception has been thrown");
+
+        WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        });
+
+        return element;
+    }
+
     public void clickOn(By locator) {
-        DriverWrapper.getDriver().findElement(locator).click();
+        webAction(locator).click();
     }
 
     public void setValue(By locator, String text) {
-        DriverWrapper.getDriver().findElement(locator).sendKeys(text);
+        webAction(locator).sendKeys(text);
     }
 
     public String getTextFromElement(By locator) {
